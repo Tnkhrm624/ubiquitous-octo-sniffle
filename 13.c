0affef48b8282df675a0658094252049cc12e1d1
Tnkhrm624 <t246965@st.yamagata-u.ac.jp>
2025-10-24 19:23:16 +0900

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<limits.h>
+#include<float.h>
+int main(void)
+{
+    printf("double型の最大値:%d\n",DBL_MAX);
+    printf("double型の最小値:%g\n",DBL_MIN);
+    printf("double型の精度:%d\n",DBL_DIG);
+    printf("int型の最大値:%g\n",INT_MAX);
+    printf("int型の最小値:%d\n",INT_MIN);
+    
+    return 0;
+}
